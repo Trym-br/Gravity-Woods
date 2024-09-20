212177f44445e9eebc3c049213adddb96b171cf1
Trym-br <trym.brandal.arhaug@gmail.com>
2024-09-20 10:38:07 +0200

    Added camera follow

---
+++ Assets/Scripts/CameraFollowPlayer.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour
+{
+    public Transform player;
+    // Update is called once per frame
+    void Update () {
+        transform.position = player.transform.position + new Vector3(0, 1, -5);
+    }
+}
